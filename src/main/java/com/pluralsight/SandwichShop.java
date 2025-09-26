f3eaf18a1622286410b76a35cd1770bd1c130352
praytoo <praytoo52@icloud.com>
2025-09-26 12:35:03 -0700

    SandwichShop3 is final

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -7,19 +7,31 @@ public class SandwichShop {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What size sandwich do you want?\n a. 1: Regular: base price $5.45\n" +
                 "b. 2: Large: base price $8.95");
-        String sandwich = scanner.nextLine();
+        int size = scanner.nextInt();
+                scanner.nextLine();
+
+        double baseprice = 0;
+
+        if (size == 1){
+            baseprice = 5.45;
+        }else if (size == 2){
+            baseprice = 8.95;
+        }
         System.out.println("How old are you?\n a. Student (17 years old or younger) — receive a 10% discount\n" +
                 "b. Seniors (65 years old or older) — receive a 20% discount");
         int age = Integer.parseInt(scanner.nextLine());
-        double sandwich1 = 5.45;
-        double sandwich2 = 8.95;
+        double discount = 0;
         if (17 >= age) {
+            discount = 0.10;
             System.out.println("receive a 10% discount:");
         } else if ( age >= 65) {
+            discount = 0.20;
             System.out.println("receive a 20% discount:");
         } else {
             System.out.println("full price");
         };
+        double finalprice = baseprice - (baseprice * discount);
 
+        System.out.println("Your sandiwch costs" + " " + "$" + finalprice);
     }
 }
